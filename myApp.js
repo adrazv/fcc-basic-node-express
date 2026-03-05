b576063fa2950dbd5c6febf1d91dfe4f291bc1c4
adrazv <234845549+adrazv@users.noreply.github.com>
2026-03-05 20:37:42 -0300

    get data from POST requests

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
+// Middleware to parse URL-encoded bodies (from HTML forms)
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Middleware to parse JSON bodies (optional, but good practice)
+app.use(bodyParser.json());
 
+// POST handler at /name
+app.post('/name', (req, res) => {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
